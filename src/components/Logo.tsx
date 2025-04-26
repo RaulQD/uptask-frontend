04cd646c6372dde0029920b4a638b92ca8bb8ffc
Raul Kevin Quispe Diaz <raul.g.quispe@gmail.com>
2025-04-26 13:00:13 -0500

    try to add seo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,9 @@
-
 export default function Logo() {
-  return (
-    <img src="/logo.svg" alt="Logotipo UpTask" width='500px' height='500px' />
-  )
+    return (
+        <img
+            src='/logo.svg'
+            alt='Logotipo UpTask'
+            style={{ width: '100%', height: 'auto' }}
+        />
+    );
 }
